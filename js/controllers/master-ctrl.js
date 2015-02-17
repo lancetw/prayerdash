abdce1e22cc402f4d64455e3bcd7349ef2b7b892
Hsin-lin Cheng <lancetw@gmail.com>
2015-02-18 03:09:23 +0800

    修正連結問題

--- js/controllers/master-ctrl.js
+++ js/controllers/master-ctrl.js
@@ -5,13 +5,13 @@
 angular.module('RDash')
 .controller('MasterCtrl', ['$scope', '$cookieStore', '$rootScope', MasterCtrl])
 .controller('MainDataCtrl', ['$scope', '$http', '$stateParams', '$rootScope', MainDataCtrl])
-.controller('DailyCtrl', ['$scope', '$http', '$stateParams', DailyCtrl])
-.controller('WeeklyCtrl', ['$scope', '$http', '$stateParams', WeeklyCtrl])
-.controller('ThisMonthCtrl', ['$scope', '$http', '$stateParams', ThisMonthCtrl])
-.controller('MonthlyCtrl', ['$scope', '$http', '$stateParams', MonthlyCtrl])
-.controller('TargetsCtrl', ['$scope', '$http', '$stateParams', TargetsCtrl])
-.controller('BustedsCtrl', ['$scope', '$http', '$stateParams', BustedsCtrl])
-.controller('InfoCtrl', ['$scope', '$http', '$stateParams', InfoCtrl]);
+.controller('DailyCtrl', ['$scope', '$http', '$stateParams', '$rootScope', DailyCtrl])
+.controller('WeeklyCtrl', ['$scope', '$http', '$stateParams', '$rootScope', WeeklyCtrl])
+.controller('ThisMonthCtrl', ['$scope', '$http', '$stateParams', '$rootScope', ThisMonthCtrl])
+.controller('MonthlyCtrl', ['$scope', '$http', '$stateParams', '$rootScope', MonthlyCtrl])
+.controller('TargetsCtrl', ['$scope', '$http', '$stateParams', '$rootScope', TargetsCtrl])
+.controller('BustedsCtrl', ['$scope', '$http', '$stateParams', '$rootScope', BustedsCtrl])
+.controller('InfoCtrl', ['$scope', '$http', '$stateParams', '$rootScope', InfoCtrl]);
 
 var SITE_URL = 'http://1and1.deliverwork.info/';
 //var SITE_URL = 'http://192.168.0.100:8000/';
@@ -52,13 +52,14 @@ function MasterCtrl($scope, $cookieStore, $http, $stateParams, $rootScope) {
 }
 
 function MainDataCtrl($scope, $http, $stateParams, $rootScope) {
+
   if (!$stateParams || $stateParams.qlink === '') {
     window.location.href = "http://1and1.ccea.org.tw";
   }
 
-  $scope.qlink = $stateParams.qlink;
+  $rootScope.qlink = $stateParams.qlink;
   $scope.data = [];
-  $http.get(SITE_URL + 'api/v1/data?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       if (angular.equals({}, data)) {
         window.location.href = "http://1and1.ccea.org.tw";
@@ -69,21 +70,21 @@ function MainDataCtrl($scope, $http, $stateParams, $rootScope) {
       $rootScope.churchData.name = '未命名';
   });
 
-  $http.get(SITE_URL + 'api/v1/data/targets?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/targets?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       $scope.targets  = data;
     }).
     error(function(data, status, headers, config) {
   });
 
-  $http.get(SITE_URL + 'api/v1/data/actions?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/actions?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       $scope.actions  = data;
     }).
     error(function(data, status, headers, config) {
   });
 
-  $http.get(SITE_URL + 'api/v1/data/users?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/users?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       $scope.users  = data;
     }).
@@ -91,13 +92,13 @@ function MainDataCtrl($scope, $http, $stateParams, $rootScope) {
   });
 }
 
-function DailyCtrl($scope, $http, $stateParams) {
+function DailyCtrl($scope, $http, $stateParams, $rootScope) {
   $scope.labels = ['0:00', '1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '23:00'];
   $scope.series = ['禱告點擊總數', '新加入守望者'];
-  $scope.qlink = $stateParams.qlink;
+  $rootScope.qlink = $stateParams.qlink;
   $scope.data = [];
 
-  $http.get(SITE_URL + 'api/v1/data/stat/today?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/stat/today?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       if (data.statistic_actions_today && data.statistic_users_today) {
         $scope.data = [
@@ -115,13 +116,13 @@ function DailyCtrl($scope, $http, $stateParams) {
   };
 }
 
-function WeeklyCtrl($scope, $http, $stateParams) {
+function WeeklyCtrl($scope, $http, $stateParams, $rootScope) {
   $scope.labels = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
   $scope.series = ['禱告點擊總數', '新加入守望者'];
-  $scope.qlink = $stateParams.qlink;
+  $rootScope.qlink = $stateParams.qlink;
   $scope.data = [];
 
-  $http.get(SITE_URL + 'api/v1/data/stat/lastweek?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/stat/lastweek?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       if (data.statistic_actions_lastweek && data.statistic_users_lastweek) {
         $scope.data = [
@@ -139,17 +140,17 @@ function WeeklyCtrl($scope, $http, $stateParams) {
   };
 }
 
-function ThisMonthCtrl($scope, $http, $stateParams) {
+function ThisMonthCtrl($scope, $http, $stateParams, $rootScope) {
   if (weekCount(moment().format('gggg'), moment().format('M')) === 4) {
     $scope.labels = ['第一週', '第二週', '第三週', '第四週'];
   } else {
     $scope.labels = ['第一週', '第二週', '第三週', '第四週', '第五週'];
   }
   $scope.series = ['禱告點擊總數', '新加入守望者'];
-  $scope.qlink = $stateParams.qlink;
+  $rootScope.qlink = $stateParams.qlink;
   $scope.data = [];
 
-  $http.get(SITE_URL + 'api/v1/data/stat/month_by_weeks?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/stat/month_by_weeks?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       if (data.statistic_actions_month_by_weeks && data.statistic_users_month_by_weeks) {
         $scope.data = [
@@ -167,13 +168,13 @@ function ThisMonthCtrl($scope, $http, $stateParams) {
   };
 }
 
-function MonthlyCtrl($scope, $http, $stateParams) {
+function MonthlyCtrl($scope, $http, $stateParams, $rootScope) {
   $scope.labels = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
   $scope.series = ['禱告點擊總數', '新加入守望者'];
-  $scope.qlink = $stateParams.qlink;
+  $rootScope.qlink = $stateParams.qlink;
   $scope.data = [];
 
-  $http.get(SITE_URL + 'api/v1/data/stat/year?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/stat/year?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       if (data.statistic_actions_year && data.statistic_users_year) {
         $scope.data = [
@@ -191,13 +192,13 @@ function MonthlyCtrl($scope, $http, $stateParams) {
   };
 }
 
-function TargetsCtrl($scope, $http, $stateParams) {
-  $scope.qlink = $stateParams.qlink;
+function TargetsCtrl($scope, $http, $stateParams, $rootScope) {
+  $rootScope.qlink = $stateParams.qlink;
   $scope.data = [];
   $scope.current_page = 1;
 
   $scope.getItems = function () {
-    $http.get(SITE_URL + 'api/v1/data/targetall/20?qlink=' + $scope.qlink + '&page=' + $scope.current_page).
+    $http.get(SITE_URL + 'api/v1/data/targetall/20?qlink=' + $rootScope.qlink + '&page=' + $scope.current_page).
       success(function(data, status, headers, config) {
         if (data.total) {
           $scope.total = data.total;
@@ -220,13 +221,13 @@ function TargetsCtrl($scope, $http, $stateParams) {
   $scope.getItems();
 }
 
-function BustedsCtrl($scope, $http, $stateParams) {
-  $scope.qlink = $stateParams.qlink;
+function BustedsCtrl($scope, $http, $stateParams, $rootScope) {
+  $rootScope.qlink = $stateParams.qlink;
   $scope.data = [];
 
 
   $scope.getItems = function () {
-    $http.get(SITE_URL + 'api/v1/data/bustedall/20?qlink=' + $scope.qlink + '&page=' + $scope.current_page).
+    $http.get(SITE_URL + 'api/v1/data/bustedall/20?qlink=' + $rootScope.qlink + '&page=' + $scope.current_page).
       success(function(data, status, headers, config) {
         if (data.total) {
           $scope.total = data.total;
@@ -248,12 +249,12 @@ function BustedsCtrl($scope, $http, $stateParams) {
   $scope.getItems();
 }
 
-function InfoCtrl($scope, $http, $stateParams) {
-  $scope.qlink = $stateParams.qlink;
+function InfoCtrl($scope, $http, $stateParams, $rootScope) {
+  $rootScope.qlink = $stateParams.qlink;
   $scope.info = {};
 
 
-  $http.get(SITE_URL + 'api/v1/data/info?qlink=' + $scope.qlink).
+  $http.get(SITE_URL + 'api/v1/data/info?qlink=' + $rootScope.qlink).
     success(function(data, status, headers, config) {
       $scope.info  = data;
     }).
